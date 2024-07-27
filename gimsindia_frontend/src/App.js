d3c94219a9a3945793345ad10d51d1e18a20a59e
KishoreRT <kishorert@applieddatafinance.com>
2024-07-27 11:42:07 +0530

    Inital diaplay of homepage

--- gimsindia_frontend/src/App.js
+++ gimsindia_frontend/src/App.js
@@ -1,49 +1,74 @@
-import React, { useEffect, useState } from 'react';
-import Login from './components/Login';
+import React from "react";
+import "./App.css";
 
 function App() {
-  const [users, setUsers] = useState([]);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const fetchUsers = (token) => {
-    fetch('http://localhost:8000/api/accounts/users/', {
-      headers: {
-        'Authorization': `Bearer ${token}`,
-      },
-    })
-      .then(response => response.json())
-      .then(data => setUsers(data))
-      .catch(error => console.error('Error fetching data:', error));
-  };
-
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      fetchUsers(token);
-      setIsLoggedIn(true);
-    }
-  }, []);
-
-  const handleLoginSuccess = () => {
-    const token = localStorage.getItem('token');
-    fetchUsers(token);
-    setIsLoggedIn(true);
-  };
-
   return (
-    <div className="App">
-      {isLoggedIn ? (
-        <>
-          <h1>Users</h1>
-          <ul>
-            {users.map(user => (
-              <li key={user.id}>{user.username} - {user.email}</li>
-            ))}
-          </ul>
-        </>
-      ) : (
-        <Login onLoginSuccess={handleLoginSuccess} />
-      )}
+    <div className="layout">
+      <div className="subtract">
+        <div className="rectangle-39455" />
+        <div className="group-1686553533">
+          <div className="ellipse-1" />
+          <div className="ellipse-2" />
+          <div className="ellipse-3" />
+          <div className="ellipse-4" />
+          <div className="ellipse-5" /> 
+          <div className="ellipse-6" />
+        </div>
+        <div className="frame-1686553869">
+          <div className="katana">KATANA</div>
+          <div className="series">Series</div>
+        </div>
+        <div className="mask-group">
+          <div className="vector-57" />
+        </div>
+        <div className="mask-group-secondary">
+          <div className="rectangle-39453" />
+        </div>
+        <div className="water-liquid-splash" />
+        <div className="frame-1686553838">
+          <div className="explore-tech">Explore the suspension tech</div>
+          <div className="katana-series">Katana Series</div>
+        </div>
+        <div className="r6" />
+        <div className="logo-container">
+          <div className="logo">
+          {/* <div className="vector-1" /> */}
+          <img src="logo.png" alt="Description" /> </div>
+          <div className="line-1" />
+          <div className="company-name">
+            Guhan Industrial and Manufacturing Solution Pvt Ltd
+          </div>
+        </div>
+        <div className="button-container">
+          <div className="frame">
+          <div class="frame-icon">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M12 3v12m0 0l5-5m-5 5l-5-5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+          </div>
+            <div className="download-button">Download Brochure</div>
+          </div>
+        </div>
+        <div className="menu-button">
+          <div className="menu-background">
+            <div className="rectangle" />
+          </div>
+          <div className="container">
+            <div className="component-7">
+              <div className="home">Home</div>
+            </div>
+            <div className="component-8">
+              <div className="menu">Menu</div>
+            </div>
+            <div className="component-9">
+              <div className="contact">Contact</div>
+            </div>
+            <div className="component-10">
+              <div className="about-us">About us</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
