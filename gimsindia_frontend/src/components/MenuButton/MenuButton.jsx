e261fa086e8d46a83b574f1a628fe08598df6d4a
Divyalakshmi N <divyalakshmin@applieddatafinance.com>
2024-08-01 18:50:15 +0530

    Menu button fixes

--- gimsindia_frontend/src/components/MenuButton/MenuButton.jsx
+++ gimsindia_frontend/src/components/MenuButton/MenuButton.jsx
@@ -1,20 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import "./menu-button.css";
 
 export const MenuButton = ({ className }) => {
+  const [selectedMenu, setSelectedMenu] = useState("Home");
+
+  const handleMenuClick = (menu) => {
+    setSelectedMenu(menu);
+  };
   return (
     <div className={`menu-button ${className}`}>
       <div className="element">
-        <div className="component">
-          <div className="text-wrapper">Home</div>
+
+        <div className={`component ${selectedMenu === 'Home' ? 'active' : ''}`} onClick={() => handleMenuClick('Home')}>
+          <div className="text-wrapper-2">Home</div>
         </div>
-        <div className="div">
+        <div className={`div ${selectedMenu === 'Menu' ? 'active' : ''}`} onClick={() => handleMenuClick('Menu')}>
           <div className="text-wrapper-2">Menu</div>
         </div>
-        <div className="component-2">
+        <div className={`component-2 ${selectedMenu === 'Contact' ? 'active' : ''}`} onClick={() => handleMenuClick('Contact')}>
           <div className="text-wrapper-2">Contact</div>
         </div>
-        <div className="component-2">
+        <div className={`component-2 ${selectedMenu === 'About us' ? 'active' : ''}`} onClick={() => handleMenuClick('About us')}>
           <div className="text-wrapper-2">About us</div>
         </div>
       </div>
